246284cb36a2b54acb27d1eb9f113fb1e13c6922
Alex <melissasalexandros@gmail.com>
2019-12-21 19:28:28 +0200

    dhmioyrgia json arxeiou,arxikopoihsh vashs

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="main.css">
+  <title>Uno Game</title>
+</head>
+<body>
+   <body class="game">
+   <h1 class="p1">Player 1</h1>
+   <h1 class="p2">Player 2</h1>
+  
+   <table class="uno_table">
+   <th>
+    Uno Table
+   </th>
+   <tr>
+     <td class="card_1">Card 1</td>
+     <td class="card_2">Card 2</td>
+     <td class="card_3">Card 3</td>
+     <td class="card_4">Card 4</td>
+     <td class="card_5">Card 5</td>
+     <td class="card_6">Card 6</td>
+     <td class="card_7">Card 7</td>
+   </tr>
+   <tr>
+     <tr></tr>
+   </tr>
+</table>
+   <div class="controls">
+   <button>Create Game</button>
+   <button>Save Game</button>
+   <button>Load Game</button>
+</div>
+   
+
+    </body>
+</body>
+</html>
