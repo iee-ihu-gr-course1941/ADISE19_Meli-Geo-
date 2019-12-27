b07e23427d41324e1a994e1a27e9b44fa9360a3e
TasaGeo <56412064+TasaGeo@users.noreply.github.com>
2019-12-27 17:19:00 +0200

    trying something different

--- login/index.php
+++ login/index.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
 
 <html>
 
@@ -18,7 +20,7 @@
 		<form action="process.php" method="POST">
 			<p>
 				<label>Username:</label>
-				<input type="text" id="user" name="user"/>
+				<input type="text" id="username" name="user"/>
 
 			</p>
 
